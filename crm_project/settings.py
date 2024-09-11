37c7a126d45d41117438cf4d4753340d78f9c2b7
arunp007 <arunp4624@gmail.com>
2024-09-11 11:39:19 +0530

    Version-1

--- crm_project/settings.py
+++ crm_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os, dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,15 +83,19 @@ WSGI_APPLICATION = 'crm_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'crm_db_un56',
-        'USER': 'crm_project',
-        'HOST': 'dpg-crgimml6l47c73dvr5tg-a',
-        'PORT': '5432',
+if not DEBUG:
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-}
+
+else:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME' : BASE_DIR/ 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
